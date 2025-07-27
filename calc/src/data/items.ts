6a3d5ba117275fa07571e606d1d45baa037fa136
Chlora <158087623+Chlora@users.noreply.github.com>
2025-07-27 01:23:43 +0100

    ident

--- calc/src/data/items.ts
+++ calc/src/data/items.ts
@@ -475,13 +475,13 @@ const SV = SS.concat([
   'Auspicious Armor',
   'Ability Shield',
   'Booster Energy',
-  'Chaos Berry', //eimpp custom
+  'Chaos Berry', // eimpp custom
   'Clear Amulet',
   'Cornerstone Mask',
-  'Corroded Rock', //eimpp custom
+  'Corroded Rock', // eimpp custom
   'Covert Cloak',
   'Fairy Feather',
-  'Happiest Mask', //eimpp custom
+  'Happiest Mask', // eimpp custom
   'Hearthflame Mask',
   'Loaded Dice',
   'Malicious Armor',
@@ -491,7 +491,7 @@ const SV = SS.concat([
   'Punching Glove',
   'Lustrous Globe',
   'Griseous Core',
-  'Sticky Trap', //eimpp custom
+  'Sticky Trap', // eimpp custom
   'Strange Ball',
   'Syrupy Apple',
   'Unremarkable Teacup',
