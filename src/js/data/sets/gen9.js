72c1159a7b883f74992fee804db163b00cca8bd1
Chlora <158087623+Chlora@users.noreply.github.com>
2025-07-27 01:52:28 +0100

    att2

--- src/js/data/sets/gen9.js
+++ src/js/data/sets/gen9.js
@@ -1,5 +1,5 @@
 var SETDEX_SV = {
   "Ralts": {
     "LC Showdown Usage": {"level":5,"ability":"Trace","item":"Eviolite","nature":"Impish","evs":{"hp":212,"df":236,"sp":36},"moves":["Knock Off","Teleport","Will-O-Wisp","Wish"]}
-  }
+  },
 };
