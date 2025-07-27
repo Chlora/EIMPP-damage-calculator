47439c468eff19569c5c0618de7fe3af77c1381d
Chlora <158087623+Chlora@users.noreply.github.com>
2025-07-27 01:55:25 +0100

    att3

--- src/js/data/sets/gen9.js
+++ src/js/data/sets/gen9.js
@@ -1,5 +1,8 @@
 var SETDEX_SV = {
+  "Lileep": {
+    "LC Showdown Usage": {"level":5,"ability":"Storm Drain","item":"Eviolite","nature":"Calm","evs":{"hp":228,"sa":28,"sd":220,"sp":12},"moves":["Giga Drain","Stealth Rock","Recover","Ancient Power"]}
+  },
   "Ralts": {
     "LC Showdown Usage": {"level":5,"ability":"Trace","item":"Eviolite","nature":"Impish","evs":{"hp":212,"df":236,"sp":36},"moves":["Knock Off","Teleport","Will-O-Wisp","Wish"]}
-  },
+  }
 };
